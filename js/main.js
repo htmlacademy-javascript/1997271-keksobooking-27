bf250df380a0f9e7dce534f0ed03d22eeee1784c
Михаил <m.juckov2010@gmail.com>
2022-10-04 18:26:23 +0300

    Поправил домашнее задание

--- js/main.js
+++ js/main.js
@@ -1,27 +1,14 @@
-// eslint-disable-next-line no-unused-vars
-
 //Функция, возвращающая случайное целое число из переданного диапазона включительно.
-function getRandomInteger(min, max) {
-  if (max <= min || min < 0 || max < 0) {
+const getRandomNumber = (min, max, count = 0) => {
+  if (min === max || min < 0 || max < 0) {
     return NaN;
   }
 
-  const RANDOM_NUMBER = Math.floor(Math.random() * (max - min + 1)) + min;
-  return RANDOM_NUMBER;
-}
-
-console.log(getRandomInteger(1, 10));
-
-//Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
-function getRandomFloat(min, max, count) {
-  const NUMBER_OF_CHARACTER = count;
-
-  if (max <= min || min < 0 || max < 0 || count < 0) {
-    return NaN;
+  if (min > max) {
+    [min, max] = [max, min];
   }
 
-  const RANDOM_NUMBER = Math.random() * (max - min + 1) + min;
-  return +RANDOM_NUMBER.toFixed(NUMBER_OF_CHARACTER);
-}
+  return +(Math.random() * (max - min) + min).toFixed(count);
+};
 
-console.log(getRandomFloat(1, 10, 1));
+console.log(getRandomNumber(1, 10, 2));
