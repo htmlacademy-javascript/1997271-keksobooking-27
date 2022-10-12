910b87df14c3bb8adf878db998cb76dde91b66e5
Михаил <m.juckov2010@gmail.com>
2022-10-12 16:42:21 +0300

    Правки дз

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
-const TITLE = [
+const OFFER_COUNTER = 10;
+
+const LOCATION = {
+  lat: {
+    min: 35.65,
+    max: 35.7,
+  },
+  lng: {
+    min: 139.7,
+    max: 139.8,
+  },
+};
+
+const TITLES = [
   'Стандартная квартира в центре',
   'Большая квартирка на окрайне',
   'Квартира рядом с метро',
@@ -6,25 +19,11 @@ const TITLE = [
   'Загородный дом для отдыха',
 ];
 
-const TYPE = [
-  'palace',
-  'flat',
-  'house',
-  'bungalow',
-  'hotel',
-];
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 
-const CHECKIN = [
-  '12:00',
-  '13:00',
-  '14:00',
-];
+const CHECK_IN_TIMES = ['12:00', '13:00', '14:00'];
 
-const CHECKOUT = [
-  '12:00',
-  '13:00',
-  '14:00',
-];
+const CHECKOUT_TIMES = ['12:00', '13:00', '14:00'];
 
 const FEATURES = [
   'wifi',
@@ -32,9 +31,10 @@ const FEATURES = [
   'parking',
   'washer',
   'elevator',
+  'conditioner',
 ];
 
-const DESCRIPTION = [
+const DESCRIPTIONS = [
   'Просто маленькая квартирка для бедных студентов',
   'Дворец для настоящих ценителей архитектуры',
   'Подвальчик в одной из пятиэтажек',
@@ -50,7 +50,6 @@ const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 
-
 const getRandomNumber = (min, max, count = 0) => {
   if (min === max || min < 0 || max < 0) {
     return NaN;
@@ -60,22 +59,25 @@ const getRandomNumber = (min, max, count = 0) => {
     [min, max] = [max, min];
   }
 
-  return +(Math.random() * (max - min ) + min).toFixed(count);
+  return +(Math.random() * (max - min) + min).toFixed(count);
 };
 
 //Функция добаления нуля однозначным числам
-const padValue = (value) => {
+const createAvatarSrc = (value) => {
   if (value < 10) {
-    return `0${value}`;
+    value = `0${value}`;
   }
-  return value;
+  return `img/avatars/user${value}.png`;
 };
 
 //Создание массива из случайных значений
-const getRandomArrayLength = ([...arrays] , maxLength) => Array.from(
-  { length : Math.min(arrays.length, 1 + Math.random() * maxLength | 0) },
-  () => arrays.splice(Math.random() * arrays.length | 0, 1)[0]
-);
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
 
 //Функция для получения случайного элемента массива
 const getRandomArrayElement = (elements) =>
@@ -83,39 +85,37 @@ const getRandomArrayElement = (elements) =>
 
 //Создание объекта
 const createObject = () => {
-  const lat = getRandomNumber(35.65000,35.70000,5);
-  const lng = getRandomNumber(139.70000, 139.80000 ,5);
-  return{
-    avatar : {
-      avatar: `img/avatars/user${padValue(getRandomNumber(1, 10))}.png`,
+  const location = {
+    lat: getRandomNumber(LOCATION.lat.min, LOCATION.lat.max, 5),
+    lng: getRandomNumber(LOCATION.lng.min, LOCATION.lng.max, 5),
+  };
+
+  return {
+    author: {
+      avatar: createAvatarSrc(getRandomNumber(1, 10)),
     },
-    offer : {
-      title : getRandomArrayElement(TITLE),
-      adress : {
-        lat,
-        lng,
-      },
-      price : getRandomNumber(1, 100000),
-      type : getRandomArrayElement(TYPE),
-      rooms : getRandomNumber(1, 5),
-      checkin : getRandomArrayElement(CHECKIN),
-      checkout : getRandomArrayElement(CHECKOUT),
-      features : getRandomArrayLength(FEATURES,getRandomNumber(1,5)),
-      description : getRandomArrayElement(DESCRIPTION),
-      photos : getRandomArrayLength(PHOTOS,getRandomNumber(1,3)),
+    offer: {
+      title: getRandomArrayElement(TITLES),
+      address: `${location.lat},${location.lng}`,
+      price: getRandomNumber(1, 100000),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomNumber(1, 5),
+      guests: getRandomNumber(1, 3),
+      checkin: getRandomArrayElement(CHECK_IN_TIMES),
+      checkout: getRandomArrayElement(CHECKOUT_TIMES),
+      features: shuffleArray(FEATURES).slice(
+        getRandomNumber(1, FEATURES.length)
+      ),
+      description: getRandomArrayElement(DESCRIPTIONS),
+      photos: shuffleArray(PHOTOS).slice(getRandomNumber(1, PHOTOS.length)),
     },
-    location : {
-      lat,
-      lng
-    }
+    location,
   };
 };
 
-
 //Создание массива
 
-const similarOffer = () => Array.from({length:10}, createObject);
+const CreateSimilarOffers = (counter) =>
+  Array.from({ length: counter }, createObject);
 
-similarOffer();
-
-
+console.log(CreateSimilarOffers(OFFER_COUNTER));
